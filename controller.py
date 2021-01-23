7f6075bc86409d19e8bf80d396ca239cbdd3b861
Woosang Kang <wk228@cornell.edu>
2021-01-23 11:23:25 +0900

    added basic configurations for each class

--- controller.py
+++ controller.py
@@ -1,39 +1,12 @@
 import datetime
-from enum import Enum
-from typing import Mapping, Tuple
-
-
-class Bank:
-    ACC_TYPES = ["savings", "checking"]
-
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.accounts = {}
-
-    def create_account(
-        self, holder: str, acc_type: str, pin: int, balance: int
-    ) -> None:
-        acc = Account(holder, acc_type, pin, balance)
-        card_num = self.genereate_card_num()
-        self.accounts[card_num] = acc
-
-    def validate(self, credentials: Mapping[str, int]) -> bool:
-        pass
-
-    def transaction(self, sender: Account, recepient: Account, amt: int) -> None:
-        pass
-
-    def genereate_card_num(self) -> int:
-        return 0
+from typing import Mapping
 
 
 class Account:
-    def __init__(
-        self, holder: str, acc_type: str, acc_num: int, pin: int, balance=0
-    ) -> None:
+    def __init__(self, holder: str, acc_type: str, card_num: int, balance=0) -> None:
         self.holder = holder
         self.acc_type = acc_type
-        self.acc_num = acc_num
+        self.card_num = card_num
         self.balance = balance
         self.active = True
         self.transactions = {}
@@ -42,20 +15,52 @@ class Account:
         pass
 
 
+class Bank:
+    ACC_TYPES = ["savings", "checking"]
+    ACC_COUNT = 0
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.accounts = {}
+
+    def create_account(
+        self, holder: str, acc_type: str, pin: int, balance: int
+    ) -> None:
+        acc = Account(holder, acc_type, balance)
+        card_num = self.genereate_card_num()
+        self.accounts[(card_num, pin)] = acc
+        Bank.ACC_COUNT += 1
+
+    def validate(self, card_num: int, pin: int) -> Account:
+        return (card_num, pin) in self.accounts
+
+    def transaction(self, sender: Account, recepient: Account, amt: int) -> None:
+        pass
+
+    def genereate_card_num(self) -> int:
+        str_num = str() + str(Bank.ACC_COUNT)
+        return str.zfill(str_num)
+
+
 class Controller:
     THRESHOLD = 3
 
     def __init__(self, bank: Bank, init_cash: int) -> None:
         self.bank = bank
         self.cash_bin = init_cash
-        self.current_user = None
-        self.wrong_dount = 0
+        self.current_user: Account = None
+        self.wrong_count = 0
 
-    def validate(self, credentials: Mapping[str, int]) -> bool:
-        pass
+    def validate(self, card_num: int, pin: int) -> Account:
+        return self.bank.validate(card_num, pin)
+
+    def initiate(self, card_num, pin) -> tuple(int, str):
+        if self.validate(card_num, pin):
+            pass
 
     def check_balance(self):
-        pass
+        if self.current_user:
+            return self.current_user.balance
 
     def deposit(self, amt):
         pass
@@ -67,7 +72,9 @@ class Controller:
         pass
 
     def deactivate(self) -> None:
-        pass
+        if self.current_user:
+            self.current_user.active = False
 
     def end(self) -> tuple(int, str):
-        pass
+        self.current_user = None
+        self.wrong_count = 0
